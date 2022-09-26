9428c38eacb7f71ff01fb2bdd4e9822d955967cf
Michał Walczak <michalwalczak777@gmail.com>
2022-09-27 00:26:53 +0200

    Add API request for a list of most popular cryptocurrencies in "Dashboard" and pass it to "ChartGrid" instead of previously used mock list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Dashboard from "./components/stockDashboard/dashboard";
 import logo from "./logo.svg";
-import { cryptocurrencies } from "./utils";
 
 function App() {
   return (
     <div>
-      <Dashboard cryptocurrencies={cryptocurrencies} />
+      <Dashboard/>
     </div>
   );
 }
--- src/components/stockDashboard/dashboard/index.tsx
+++ src/components/stockDashboard/dashboard/index.tsx
@@ -1,12 +1,25 @@
-import Chart from "../chart";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import ChartGrid from "../chartGrid";
 
-interface IDashboardProps {
-  cryptocurrencies: Array<string>;
-}
+const Dashboard = () => {
+  const requestedCurrenciesAmount: number = 10;
+  const [currencies, setCurrencies] = useState<Array<string>>([]);
 
-const Dashboard = ({ cryptocurrencies }: IDashboardProps) => {
-  return <ChartGrid cryptocurrencies={cryptocurrencies} />;
+  useEffect(() => {
+    const getCurrenciesByMarketCap = async () => {
+      const response = await axios.get(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=${requestedCurrenciesAmount}&page=1&sparkline=false`
+      );
+      const curreciesIDs: Array<string> = response.data.map(
+        (currencyData: { id: string }) => currencyData.id
+      );
+      setCurrencies(curreciesIDs);
+    };
+    getCurrenciesByMarketCap();
+  }, []);
+
+  return <ChartGrid cryptocurrencies={currencies} />;
 };
 
 export default Dashboard;
